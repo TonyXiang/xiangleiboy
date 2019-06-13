e6303e15bf26605111db05cce9a370a2f0892992
TonyXiang <15067180409xl@gmail.com>
2019-06-13 17:22:31 +0800

    更新

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var path = require('path')
 var app = express()
 
 app.use(express.static(path.join(__dirname, 'docs')))
-app.listen(3000, function() {
-  console.log('serve start at port 8080');
+app.listen(4000, function() {
+  console.log('serve start at port 4000');
 });
